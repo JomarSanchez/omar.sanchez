4ba534ff483d965fdc2105f7c415b324a720bf59
omar <omar.sanchez201999@gmail.com>
2020-08-14 23:54:23 -0500

    Primer deploy

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { } from "../src/js/navegation.js";
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
+import './styles.css'
---
+++ src/js/navegation.js
@@ -0,0 +1,13 @@
+
+const btnMenu = document.querySelector(".menuToggle");
+const nav = document.querySelector('.listMenu');
+
+
+btnMenu.addEventListener('click', ()=>{
+    nav.classList.toggle('active')
+    nav.classList.toggle('show')
+})
+
+    
+
+
